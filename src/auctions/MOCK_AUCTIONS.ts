2b4f61d210c83347d5d31255bdcf8ce59f17b5d0
deerlover <apadki27@gmail.com>
2024-01-17 13:34:42 -0500

    Commit until the auctions are shown from list componenet

---
+++ src/auctions/MOCK_AUCTIONS.ts
@@ -0,0 +1,30 @@
+import { Auction } from './Auction';
+
+export const MOCK_AUCTIONS = [
+  new Auction({
+    id: 1,
+    name: 'Dinner at Padkis',
+    description:
+      'Dinner at Padkis',
+    imageUrl: 'https://picsum.photos/200/100', 
+    offeredBy: 'Anu Padki',
+    numberOfOffers: 4,
+    initialBid: 50,
+    finalBid: 0,
+    bidders:  [],
+    offerdOn: new Date("2024-11-22")
+  }),
+  new Auction({
+    id: 1,
+    name: 'Wine and Poetry at Padkis',
+    description:
+      'Wine and Read Poems',
+    imageUrl: 'https://picsum.photos/200/100', 
+    offeredBy: 'Milind Padki',
+    numberOfOffers: 4,
+    initialBid: 50,
+    finalBid: 0,
+    bidders:  [],
+    offerdOn: new Date("2024-12-22")
+  })
+];
